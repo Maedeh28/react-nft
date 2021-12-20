4d11ef358dc2d3670f3805082bbc588fa3ff931b
Maedeh Mousavi <maedeh.mousavi@finshark.io>
2021-12-20 01:35:19 +0100

    working on owner component/css

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Header from './components/Header';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import PunkList from './components/PunkList';
+import Main from './components/Main';
 
 function App() {
   const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0);
 
   useEffect(() => {
     const getMyNfts = async () => {
-      const openSeaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x4c1d82f6C4F56922465BA141657585ca57FCabDf')
+      const openSeaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x4c1d82f6C4F56922465BA141657585ca57FCabDf&order_direction=asc')
       console.log(openSeaData);
       setPunkListData(openSeaData.data.assets)
     }
@@ -19,7 +21,12 @@ function App() {
   return (
     <div className='app'>
       <Header />
-      <PunkList punkListData={punkListData} />
+      {punkListData.length > 0 && (
+        <>
+          <Main selectedPunk={selectedPunk} punkListData={punkListData} />
+          <PunkList punkListData={punkListData} setSelectedPunk={setSelectedPunk} />
+        </>
+      )}
     </div>
   )
 }
